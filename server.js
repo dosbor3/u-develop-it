6c19d90993e9e1e5bf7239c690906e9d224638ba
I am Billie <dosbor3@gmail.com>
2022-02-14 14:08:13 -0600

    branch switch not made

--- server.js
+++ server.js
@@ -19,17 +19,35 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => { //request for all potential candidates
-//   console.log(rows);
-// });
+//request for all potential candidates
+// Get all candidates
+// Instead of logging the error, we'll send a status code of 500 and place 
+// the error message within a JSON object. This will all be handled within 
+// the error-handling conditional. The 500 status code indicates a server 
+// error—different than a 404, which indicates a user request error. The 
+// return statement will exit the database call once an error is encountered.
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 //Query for a READ Operation 
-db.query("SELECT * FROM candidates WHERE id = 1", (err, row) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(row);
-});
+// db.query("SELECT * FROM candidates WHERE id = 1", (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
 
 // //Query for DELETE Operation
 // db.query("DELETE FROM candidates WHERE id = ?", 1, (err, result) => {
@@ -41,16 +59,16 @@ db.query("SELECT * FROM candidates WHERE id = 1", (err, row) => {
 
 //Query for CREATE Operation
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 
 
