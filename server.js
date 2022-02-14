e183ef19b6a39097c94e387b99c9bea8d5d3b8d0
I am Billie <dosbor3@gmail.com>
2022-02-14 15:18:56 -0600

    add CRUD Routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -41,21 +42,80 @@ app.get('/api/candidates', (req, res) => {
   });
 });
 
-//Query for a READ Operation 
-// db.query("SELECT * FROM candidates WHERE id = 1", (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+// Let's analyze the route. Here we're using the get() route method again. This time, 
+// the endpoint has a route parameter that will hold the value of the id to specify 
+// which candidate we'll select from the database.
 
-// //Query for DELETE Operation
-// db.query("DELETE FROM candidates WHERE id = ?", 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+// In the database call, we'll assign the captured value populated in the req.params 
+// object with the key id to params. The database call will then query the candidates 
+// table with this id and retrieve the row specified. Because params can be accepted in 
+// the database call as an array, params is assigned as an array with a single element, req.params.id.
+
+// The error status code was changed to 400 to notify the client that their request wasn't 
+// accepted and to try a different request. In the route response, we'll send the row back to 
+// the client in a JSON object.
+
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+});
+});
 
 //Query for CREATE Operation
 // Create a candidate
